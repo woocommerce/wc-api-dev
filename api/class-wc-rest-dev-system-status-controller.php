1dc35711b05f2fbb8f2cd0198d889dcfb1ae2beb
Justin Shreve <justin.shreve@gmail.com>
2017-07-12 09:06:14 -0700

    Revert "Return in the system status report if a site has run the setup wizard (#24)" (#25)
    This reverts commit 394c65bed0d6a4a5cd0a8e3c87399d4742c4c2aa.

--- api/class-wc-rest-dev-system-status-controller.php
+++ api/class-wc-rest-dev-system-status-controller.php
@@ -26,20 +26,4 @@ class WC_REST_Dev_System_Status_Controller extends WC_REST_System_Status_Control
 	 */
 	protected $namespace = 'wc/v3';
 
-	/**
-	 * Returns setting values for the site that are useful for debugging
-	 * purposes. For full settings access, use the settings api.
-	 *
-	 * @return array
-	 */
-	public function get_settings() {
-		$settings       = parent::get_settings();
-		$settings_extra = array();
-		$admin_notices  = get_option( 'woocommerce_admin_notices', array() );
-
-		$settings_extra['setup_wizard_ran'] = ! ( in_array( 'install', $admin_notices ) );
-
-		return array_merge( $settings, $settings_extra );
-	}
-
 }
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: automattic, woothemes
 Tags: woocommerce, rest-api, api
 Requires at least: 4.6
 Tested up to: 4.8
-Stable tag: 0.7.0
+Stable tag: 0.6.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -24,9 +24,6 @@ This section describes how to install the plugin and get it working.
 1. Activate the plugin through the 'Plugins' screen in WordPress
 
 == Changelog ==
-
-= 0.7.0 =
-* Return in the system status report if a store has gone through the setup wizard before
 
 = 0.6.0 =
 * Fix value default return on settings endpoints
