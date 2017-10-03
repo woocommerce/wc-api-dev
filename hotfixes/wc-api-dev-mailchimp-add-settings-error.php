e69ceaade5bf616a4bde8fb3f09b894a67834f53
Bartosz <bartosz.budzanowski@gmail.com>
2017-10-02 18:03:23 -0700

    Add mocked add_settings_error.

---
+++ hotfixes/wc-api-dev-mailchimp-add-settings-error.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * add_settings_error is only available in UI contecext
+ * adding a mock version to prevent API failures
+ *
+ * @since 0.8.8
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+//TODO:
+//  - use this function to capture messages and feed them back to Calypso on return from API.
+
+if ( ! function_exists( 'add_settings_error' ) ) {
+        function add_settings_error( $setting, $code, $message, $type = 'error' ) {}
+}
--- wc-api-dev-class.php
+++ wc-api-dev-class.php
@@ -115,6 +115,7 @@ class WC_API_Dev {
 			include_once( dirname( __FILE__ ) . '/hotfixes/wc-api-dev-allowed-redirect-hosts.php' );
 			include_once( dirname( __FILE__ ) . '/hotfixes/wc-api-dev-masterbar-menu.php' );
 			include_once( dirname( __FILE__ ) . '/hotfixes/wc-api-dev-mailchimp-no-redirect.php' );
+			include_once( dirname( __FILE__ ) . '/hotfixes/wc-api-dev-mailchimp-add-settings-error.php' );
 		}
 
 		// Classes that are not related to the API.
